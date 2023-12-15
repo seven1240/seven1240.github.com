cce3e4175554e4959fd736d4e71f91875cb5e68b
Seven Du <dujinfang@x-y-t.cn>
2023-12-15 08:26:21 +0800

    fix link

--- bin/_new.sh
+++ bin/_new.sh
@@ -7,7 +7,7 @@ F=../_posts/`date +%Y-%m-%d-`$1.md
 cp _template $F
 
 echo >> $F
-echo "本文永久链接：<https://www.dujinfang.com/`date +%Y-%m-%d/`$1.html> 。" >> $F
+echo "本文永久链接：<https://www.dujinfang.com/`date +%Y/%m/%d/`$1.html> 。" >> $F
 echo >> $F
 
 echo $F done!
